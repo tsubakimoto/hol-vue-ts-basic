35505fe1356558f4c65e54bf2e24b40323740204
tsubakimoto <tkmbmbr@gmail.com>
2019-06-01 16:50:34 +0900

    opt02まで進めた

--- src/vue-lab/src/views/Home.vue
+++ src/vue-lab/src/views/Home.vue
@@ -5,18 +5,27 @@
 
     <h3>リストレンダリング</h3>
     <ul id="item-list">
-      <li v-for="item in items" :key="item.id">
-        {{ item.name }}
-      </li>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
     </ul>
 
     <h3>イベントハンドリング</h3>
     <button v-on:click="onClick">実行</button>
+
+    <h3>算出プロパティ</h3>
+    <p>{{ computedMsg }}</p>
+
+    <h3>条件付きレンダリング</h3>
+    <button v-on:click="showDetail">表示</button>
+    <div v-if="detail">詳細が表示されました。</div>
+
+    <h3>エラー</h3>
+    <button v-on:click="raiseError">エラー</button>
+    <div v-bind:class="{ 'text-danger': hasError}">システムからのメッセージ</div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 declare interface Item {
   id: number;
@@ -36,12 +45,25 @@ export default Vue.extend({
           id: 2,
           name: "マウス"
         }
-      ] as Item[]
+      ] as Item[],
+      detail: false,
+      hasError: false
     };
   },
   methods: {
     onClick(): void {
       alert(this.msg);
+    },
+    showDetail(): void {
+      this.detail = true;
+    },
+    raiseError(): void {      
+      this.hasError = true;
+    }
+  },
+  computed: {
+    computedMsg(): string {
+      return this.msg.replace(/!/g, "") + " + TypeScript!";
     }
   }
 });
@@ -50,5 +72,8 @@ export default Vue.extend({
 <style scoped>
 .home {
   text-align: left;
+}
+.text-danger {
+  color: darkred;
 }
 </style>
